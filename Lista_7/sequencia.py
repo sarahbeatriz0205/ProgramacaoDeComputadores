9009e18bf3ad4a7205234c3e9c53734fcd487099
sarahbeatriz0205 <sarah.nascimento02052007@gmail.com>
2025-07-02 20:54:13 -0300

    incompleto

---
+++ Lista_7/sequencia.py
@@ -0,0 +1,10 @@
+tamanho_sequencia = int(input())
+sequencia = []
+sequencia[0] = 1
+sequencia[tamanho_sequencia] = 1
+
+for i in range(1, tamanho_sequencia):
+    sequencia[i] = int(input())
+    if sequencia[i]
+
+#INCOMPLETO
